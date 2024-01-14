d9a10dbf1148d3303fab768d276a3f9b122db903
olive027 <blueblueww@yahoo.co.jp>
2024-01-15 00:03:42 +0900

    240114

--- src/js/script.js
+++ src/js/script.js
@@ -9,33 +9,59 @@ $('.js-hamburger').click(function(){
 })
 
 //mv
-var subTitle = document.querySelectorAll('.js-subtitle');
-subTitle.forEach((target) => {
+var Title = document.querySelectorAll('.js-title');
+Title.forEach((target) => {
 	// h2タグ内のテキストを一文字ずつ分割
 	target.innerHTML = target.textContent.replace(/\S/g,'<span>$&</span>')
 });
-gsap.utils.toArray(subTitle).forEach((t) => {
-
-	gsap.fromTo( t.querySelectorAll("span"),{ //アニメーションを適用するspan要素を取得
-		autoAlpha: 0,
-		x: "-100%",
-	},{
-		delay: 0.5,
+gsap.utils.toArray(Title).forEach((t) => {
+	gsap.to( t.querySelectorAll("span"),{ //アニメーションを適用するspan要素を取得
+		delay: 0.8,
 		autoAlpha: 1,
 		x: 0,
 		stagger: 0.08,
 	});
 });
 
-gsap.fromTo('.js-title', 1, {
-	autoAlpha: 0,
-	y: '100%',
-},{
+gsap.to('.js-header', 0.8, {
 	delay: 1.2,
 	autoAlpha: 1,
 	y: 0,
+})
+
+//スクロールしたらheaderに背景色
+var Header = $(".header");
+$(window).scroll(function(){
+	if($(this).scrollTop() > 200) {
+		// それを過ぎたら付与
+		Header.addClass('is-color');
+	} else {
+		Header.removeClass('is-color');
+	}
 });
 
+// ========================== ページトップボタン============================
+$(function () {
+  const pageTop = $(".pagetop");
+  pageTop.hide(); // 最初はボタンを非表示にする
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) { // 100pxスクロールしたら表示
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+  });
+
+	pageTop.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0, // 上から0pxの位置に戻る
+      },
+      500 // 500ミリ秒かけて戻る
+    );
+    return false;
+  });
+});
 
 
 
